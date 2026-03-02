6f7fd254b81c3c8302e3e1d3d0a0c5390a4bf38c
Alvin_white <1503948873@qq.com>
2026-03-02 11:14:29 +0800

    删除未使用的Java后端代码

--- backend/src/main/java/com/education/repository/TeamMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.education.repository;
-
-import com.education.entity.TeamMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
-}
